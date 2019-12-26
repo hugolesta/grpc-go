96b030cda8ff8a7fc250a9d9f6244c8c73497e18
Hugo Lesta <hlesta@edrans.com>
2019-12-26 12:55:26 -0300

    Added protoconfig for client streaming

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -41,6 +41,27 @@ func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb
 	return nil
 }
 
+func (*server) LongGreet(stream greetpb.GreetService_LongGreetServer) error {
+	fmt.Printf("LongGreet function was invoked with a streaming request")
+	result := ""
+	for {
+		req, err != stream.Recv()
+		if err == io.EOF {
+			// we have finished reading the client stram
+			return stream.SendAndClose(&greetpb.LongGreetResponse{
+				Result: result,
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v", err)
+		}
+
+		firstName := req.GetGreeting().GetFirstName()
+		result += "Hello " + firstName + "! "
+	}	
+}
+
 func main() {
 	fmt.Println("Hello world")
 
